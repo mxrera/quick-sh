bc5e7a6ea2c93416051063833975e2e07b2b766f
mxrera <mxrera.dev@gmail.com>
2024-05-04 20:26:06 +0200

    fix: print of packages to be installed

--- install.sh
+++ install.sh
@@ -24,7 +24,8 @@ main() {
     
     whiptail --title "Welcome" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --msgbox "Welcome to QuickSH, a package installation assistant!\n\n" 8 60
 
-    general_packages=($(whiptail --title  "SELECT GENERAL PACKAGES TO INSTALL" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
+    packages=()
+    packages+=($(whiptail --separate-output --title  "SELECT GENERAL PACKAGES TO INSTALL" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
                 "List of packages" 20 100 10 \
                 "brave" "browser" OFF \
                 "gimp" "image editor" OFF \
@@ -34,8 +35,9 @@ main() {
                 "obs-studio" "screen casting and streaming app" OFF \
                 "spotify" "music player" OFF \
                 "discord" "chat" OFF \
+                "anydesk" "remote desktop" OFF \
                  3>&1 1>&2 2>&3))
-    dev_packages=($(whiptail --title "SELECT DEV PACKAGES TO INSTALL" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
+    packages+=($(whiptail --separate-output --title "SELECT DEV PACKAGES TO INSTALL" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
                 "List of packages" 20 100 10 \
                 "net-tools" "networking tools" OFF \
                 "docker" "container development" OFF \
@@ -45,29 +47,28 @@ main() {
                 "wireshark" "network protocol analyzer" OFF \
                 "foxglove-studio" "ROS visualizer" OFF \
                  3>&1 1>&2 2>&3))
-    terminal_packages=($(whiptail --title "SELECT TERMINAL PACKAGES TO INSTALL" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
+    packages+=($(whiptail --separate-output --title "SELECT TERMINAL PACKAGES TO INSTALL" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
                 "List of packages" 20 100 10 \
                 "neovim" "code editor" OFF \
                 "tmux" "terminal multiplexer" OFF \
                 "neofetch" "system information" OFF \
                 "tree" "directory tree" OFF \
+                "xclip" "clipboard manager" OFF \
                 "netcat" "network utility" OFF \
                 "tcpdump" "network packet analyzer" OFF \
                 "iputils" "network utility" OFF \
                  3>&1 1>&2 2>&3))
-
-    packages=${general_packages[@]}${dev_packages[@]}${terminal_packages[@]}
 
     if [[ ${#packages[@]} -eq 0 ]]; then
         whiptail --title "MESSAGE" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --msgbox "No packages selected. Exiting the script!" 8 78
         exit 1
     fi
 
-    whiptail --title "CONFIRMATION" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --yesno "The following packages will be installed: \n ${packages[@]}\n\nDo you want to proceed?" 20 60
-    if [[ $? -eq 0 ]]; then
+    package_string=$(printf "%s\n" "${packages[@]}")
+    if whiptail --scrolltext --title "CONFIRMATION" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --yesno "The following packages will be installed: \n\n$package_string\n\nDo you want to proceed?" 20 60; then
         clear
         install_packages ${packages[@]}
-    elif [[ $? -eq 1 ]]; then 
+    else
         whiptail --title "MESSAGE" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --msgbox "Exiting the script without installing any packages!" 8 78
         exit 1
     fi
@@ -76,7 +77,6 @@ main() {
 install_packages() {
     for package in $@; do
         echo -e "${green_color}[ INFO ]${reset_color} Installing $package ..."
-        package=$(echo $package | tr -d '"') # extract the package name from the quotes
         $package
         echo -e "${green_color}[ INFO ]${reset_color} End of $package installation."
     done
